1f0f69ee89c4fa5b69612de761faa7b559f2440f
Mh Yusuf <mhyusufibrahim@gmail.com>
2020-01-03 10:38:20 +0700

    Sampai disini HelloConfig.java sudah kosong karena semua pindah ke spring-config.xml

--- src/main/java/com/ibrahim/config/HelloConfig.java
+++ src/main/java/com/ibrahim/config/HelloConfig.java
@@ -14,37 +14,36 @@ import org.springframework.context.annotation.Profile;
 public class HelloConfig {
 
     // Dengan factory pattern
-    @Bean
-    public HelloWorldFactory helloWorldFactory() {
-        return new HelloWorldFactory();
-    }
+    // @Bean
+    // public HelloWorldFactory helloWorldFactory() {
+    //     return new HelloWorldFactory();
+    // }
 
-    @Bean
-    @Primary
-    @Profile({"default", "indo"})
-    public HelloWorldService helloWorldIndoImpl() {
-        return new HelloWorldIndoImpl();
-    }
+    // @Bean
+    // @Primary
+    // @Profile({"default", "indo"})
+    // public HelloWorldService helloWorldIndoImpl() {
+    //     return new HelloWorldIndoImpl();
+    // }
 
-    @Bean
-    @Profile({"english"})
-    public HelloWorldService helloWorldEnglishImpl() {
-        return new HelloWorldEnglishImpl();
-    }
+    // @Bean
+    // @Profile({"english"})
+    // public HelloWorldService helloWorldEnglishImpl() {
+    //     return new HelloWorldEnglishImpl();
+    // }
 
-    @Bean
-    public HelloWorldService helloWorldSundaImpl(HelloWorldFactory factory) {
-        return factory.createHelloWorldService("sunda");
-    }
+    // @Bean
+    // public HelloWorldService helloWorldSundaImpl(HelloWorldFactory factory) {
+    //     return factory.createHelloWorldService("sunda");
+    // }
 
-    @Bean
-    public HelloWorldService helloWorldJawaImpl(HelloWorldFactory factory) {
-        return factory.createHelloWorldService("jawa");
-    }
+    // @Bean
+    // public HelloWorldService helloWorldJawaImpl(HelloWorldFactory factory) {
+    //     return factory.createHelloWorldService("jawa");
+    // }
 
-    @Bean(name="padang")
-    public HelloWorldService helloWorldPadangImpl(HelloWorldFactory factory) {
-        return factory.createHelloWorldService("padang");
-    }
-
+    // @Bean(name="padang")
+    // public HelloWorldService helloWorldPadangImpl(HelloWorldFactory factory) {
+    //     return factory.createHelloWorldService("padang");
+    // }
 }
--- src/main/java/com/ibrahim/controllers/GreetingController.java
+++ src/main/java/com/ibrahim/controllers/GreetingController.java
@@ -1,15 +1,11 @@
 package com.ibrahim.controllers;
 
-import java.util.logging.Logger;
-
 import com.ibrahim.services.HelloWorldFactory;
 import com.ibrahim.services.HelloWorldService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 
-@Controller
 public class GreetingController {
 
     @Autowired
