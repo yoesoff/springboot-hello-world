7fe51225b3dce01c010e968bbdf951ae074d45ea
Mh Yusuf <mhyusufibrahim@gmail.com>
2020-01-03 08:48:10 +0700

    Belajar xml-configuration
    Memindahkan file main class kelokasi lain lalu konfig ImportResource dengan annotation disana

--- src/main/java/springboot/HaloDuniaApplication.java
+++ src/main/java/springboot/HaloDuniaApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan("com.ibrahim")
+// @ComponentScan("com.ibrahim")
+@ImportResource("classpath:/spring/spring-config.xml")
 public class HaloDuniaApplication    {
 
 	public static void main(String[] args) {
